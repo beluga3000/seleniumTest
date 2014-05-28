3d89dc16a05cec169a54261af44b95391b09870b
beluga3000 <beluga3000@gmail.com>
2014-05-28 18:25:11 +0300

    Initial commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,42 @@
+/*
+*Author - Sergey Belavsky
+*/
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import java.util.List;
+
+
+public class LoginTest {
+    WebDriver driver = new FirefoxDriver();
+
+    @Before
+    public void setUp() {
+        driver.get("https://webmail.itransition.com");
+    }
+
+    @Test
+    public void loginTest() {
+        //Выбор радиобаттона по xpath
+        WebElement radio = driver.findElement(By.xpath("//input[@value=\"4\"]"));
+        radio.click();
+        //Выбор чекбокса по CSS
+        WebElement check = driver.findElement(By.cssSelector("input#chkBsc.rdo"));
+        check.click();
+        //Выбор текстбоксов по классу
+        List<WebElement> textboxes = driver.findElements(By.className("txt"));
+        for (WebElement WE : textboxes) {
+            WE.click();
+        }
+        //Выбор кнопки входа по ID
+        WebElement button = driver.findElement(By.id("SubmitCreds"));
+        button.click();
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
